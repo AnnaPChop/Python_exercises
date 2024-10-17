551a6e7fbe057c38c009c087b8a9e128f27a48ed
AnnaPChop <annapicn@gmail.com>
2024-10-17 11:42:49 -0600

    Create exercise1.py

---
+++ exercise1.py
@@ -0,0 +1,22 @@
+'''Cuenta los thrillers en movies_info. Almacena el resultado en thrillers_count, tras lo cual el precódigo lo mostrará.'''
+#Código
+movies_info = [
+    ['The Shawshank Redemption', 'USA', 1994, 'drama', 142, 9.111],
+    ['The Godfather', 'USA', 1972, 'drama, crime', 175, 8.730],
+    ['The Dark Knight', 'USA', 2008, 'fantasy, action, thriller', 152, 8.499],
+    ["Schindler's List", 'USA', 1993, 'drama', 195, 8.818],
+    ['The Lord of the Rings: The Return of the King', 'New Zealand', 2003, 'fantasy, adventure, drama', 201, 8.625],
+    ['Pulp Fiction', 'USA', 1994, 'thriller, comedy, crime', 154, 8.619],
+    ['The Good, the Bad and the Ugly', 'Italy', 1966, 'western', 178, 8.521],
+    ['Fight Club', 'USA', 1999, 'thriller, drama, crime', 139, 8.644],
+    ['Harakiri', 'Japan', 1962, 'drama, action, history', 133, 8.106],
+    ['Good Will Hunting', 'USA', 1997, 'drama, romance', 126, 8.077]
+]
+
+thrillers_count = 0
+
+for movie in movies_info:
+    if 'thriller' in movie[3]:
+        thrillers_count += 1
+
+print(thrillers_count)
