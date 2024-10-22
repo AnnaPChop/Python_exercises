da59e95c1c34745682f90bcfbaba92f115558aaf
AnnaPChop <annapicn@gmail.com>
2024-10-22 13:32:17 -0600

    Create exercise7.py

---
+++ exercise7.py
@@ -0,0 +1,15 @@
+'''Supongamos que gestionas un presupuesto reducido y quieres controlar cuánto gastas cada día. Utilizarás la asignación aumentada para actualizar el gasto total y recurrirás a una función incorporada para calcularlo. Imprime el resultado.
+
+La salida esperada es esta:
+
+Gasto total de la semana: $ 150.75'''
+#código
+
+total_spending = 0
+#List of daily expenses
+daily_expenses = [20.5, 15.0, 22.75, 30.0, 25.0, 18.0, 19.5]
+
+#Update the total expense using augmented assigment and built-in functions
+total_expending += sum(daily_expenses)
+
+print("Gasto total de la semana: $", total_expending ) 
