03f04a14f5c2b525e9feee9486b6b295a039206b
AnnaPChop <annapicn@gmail.com>
2024-10-23 23:23:11 -0600

    Create exercise11.py

---
+++ exercise11.py
@@ -0,0 +1,30 @@
+'''Ya hemos mostrado los títulos de las películas. Intentemos mostrar los géneros de las películas de la tabla movies_info. Cada valor debe estar en una nueva línea.
+
+La salida esperada es esta:
+
+drama
+drama, crime
+fantasy, action, thriller
+drama
+fantasy, adventure, drama
+thriller, comedy, crime
+western
+thriller, drama, crime
+drama, action, history
+drama, romance'''
+#Código
+movies_info = [
+    ['The Shawshank Redemption', 'USA', 1994, 'drama', 142, 9.111],
+    ['The Godfather', 'USA', 1972, 'drama, crime', 175, 8.730],
+    ['The Dark Knight', 'USA', 2008, 'fantasy, action, thriller', 152, 8.499],
+    ["Schindler's List", 'USA', 1993, 'drama', 195, 8.818],
+    ['The Lord of the Rings: The Return of the King', 'New Zealand', 2003, 'fantasy, adventure, drama', 201, 8.625],
+    ['Pulp Fiction', 'USA', 1994, 'thriller, comedy, crime', 154, 8.619],
+    ['The Good, the Bad and the Ugly', 'Italy', 1966, 'western', 178, 8.521],
+    ['Fight Club', 'USA', 1999, 'thriller, drama, crime', 139, 8.644],
+    ['Harakiri', 'Japan', 1962, 'drama, action, history', 133, 8.106],
+    ['Good Will Hunting', 'USA', 1997, 'drama, romance', 126, 8.077]
+]
+
+for genre in movies_info:
+    print(genre[3])
