2452573c0d9b8b8aa3dc200d4a37863c00f76878
AnnaPChop <annapicn@gmail.com>
2024-11-05 15:19:37 -0600

    Create exercise13.py

---
+++ exercise13.py
@@ -0,0 +1,17 @@
+'''Tienes una variable age. Utiliza los operadores de comparación para mostrar si age cae en cada uno de estos rangos:
+
+Comprueba si age se encuentra entre 0 y 12 (inclusive) y muestra "Eres niño".
+Comprueba si age se encuentra entre 13 y 19 (inclusive) y muestra "Eres adolescente".
+Comprueba si age es 20 (inclusive) o mayor y muestra "Eres adulto".
+Completa los espacios con la expresión lógica correcta, según se explicó previamente.'''
+#Código
+age = 17
+
+#Comprobamos si la edad se encuentra entre 0 y 12 
+print( age >= o and age <= 12, "Eres niño")
+
+#Comprobamos si la edad se encuentra entre 13 y 19 
+print ( age >= 13 and age <=19, "Eres adolescente")
+
+#Comprobamos si la edad es de 20 o más
+print ( age >= 20, "Eres adulto")
